7f8814fa6a1416e7b879a1fdbab41261c298b3bd
Cristelos <cristelos@msn.com>
2023-04-16 16:42:38 +0200

    it works, trying to add some utilities

--- src/components/JuegoTrivial.jsx
+++ src/components/JuegoTrivial.jsx
@@ -5,57 +5,77 @@ export default function JuegoTrivial(){
 
     const [questions, setQuestions] = useState([]);
     const [amountQuestions, setAmountQuestions] = useState(0);
+    // const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [respuestasCorrectas, setRespuestasCorrectas] = useState(0);
 
-    //Elegimos el número de preguntas que queremos
     const handleAmoutChange = (e) => {
         setAmountQuestions(e.target.value)
     }
 
-    //Añadimos las preguntas
-    const addQuestions = () => {
-        axios.get(`https://opentdb.com/api.php?amount=${amountQuestions}`).then( res => {
-            console.log(res.data.results);
-            setQuestions(res.data.results)
-        })
-    }
+    // const handleSelect = (answer, correctAnswer) =>{
+    //     if(!selectedAnswer){
+    //         if(answer === correctAnswer){
+    //             console.log("ACERTASTE");
+    //         } else {
+    //             console.log("Fallaste");
+    //         }
+    //         setSelectedAnswer(answer);
+    //     }
+    // }
 
-    //Mezclamos las respuestas
-    const mixAnswers = (answers) => {
-        const mixedAnswers = [...answers].sort(() => Math.random() - 0.5);
-        return mixedAnswers;
-    }
+    const handleSelect = (answer, question) => {
+        if (answer === question.correct_answer) {
+          alert("¡Respuesta correcta!");
+          setRespuestasCorrectas(respuestasCorrectas + 1);
+        } else {
+        alert("Respuesta incorrecta. La respuesta correcta es: " + question.correct_answer);
+        }
+      };
 
     useEffect(() => {
-        axios.get("https://opentdb.com/api.php?amount=50").then(res => {
-            console.log(res.data.results);
-            // console.log(res);
-            setQuestions(res.results)
-        })
-    }, [])
+        if(amountQuestions > 0){
+            axios.get(`https://opentdb.com/api.php?amount=${amountQuestions}`).then(res => {
+                console.log(res.data.results);
+                setQuestions(res.data.results)
+            })
+        }
+    }, [amountQuestions])
 
     return(
         <div>
-            <input type="number" name="amount" value={amountQuestions} onChange={handleAmoutChange}/>
-            <button onClick={addQuestions}>Añade el número de preguntas</button>
-            {questions && questions.map((question, index) => {
-                
-                const answers = [ ...question.incorrect_answers, question.correct_answer];
-                
-                const mixedAnswers = mixAnswers(answers)
-
-                return (
-                    <div key={index}>
-                    <h3>{question.question}</h3>
-                    <ul>
-                        {mixedAnswers.map((answers, i) => (
-                            <li key={i}>{answers}</li>
-                        ))}
-                    </ul>
+            <div className="seleccion">
+            <h2>Indica el número de preguntas que quieras</h2>
+                <input type="number" name="amount" value={amountQuestions} onChange={handleAmoutChange}/>
+                <div className="score">
+                Respuestas correctas: {respuestasCorrectas}
                 </div>
-                )
+            </div>
+            <div className="preguntas-respuestas">
+                {questions && questions.map((question, index) => {
+                    
+                    const correctAnswer = question.correct_answer;
+                    const incorrectAnswer = question.incorrect_answers;
+                    const answers = [ ...incorrectAnswer, correctAnswer];
+                    const mixedAnswers = [...answers].sort(() => Math.random() - 0.5);
                 
-            })}
-            
+                    return (
+                        
+                        <div key={index} className="contenedor-preguntas">
+                            <h3>{question.question}</h3>
+                            <ul className="respuestas">
+                                {mixedAnswers && mixedAnswers.map((answer, i) => (
+                                    <li>
+                                        {/* <button className={answer === correctAnswer && selectedAnswer!== null ? "correct answer" : selectedAnswer !== correctAnswer && selectedAnswer!== null ? "incorrecta" : "correcta"} key={i} onClick={() => handleSelect(answer, correctAnswer)}> */}
+                                        <button key={i} onClick={() => handleSelect(answer, question)}>
+                                            {answer}
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
