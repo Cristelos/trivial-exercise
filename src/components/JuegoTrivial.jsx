5ef20e8a36a6348ce193ffb99e32fb353536c2ea
Cristelos <cristelos@msn.com>
2023-04-16 11:44:23 +0200

    adding some usabilities

--- src/components/JuegoTrivial.jsx
+++ src/components/JuegoTrivial.jsx
@@ -4,16 +4,58 @@ import { useEffect, useState } from "react"
 export default function JuegoTrivial(){
 
     const [questions, setQuestions] = useState([]);
+    const [amountQuestions, setAmountQuestions] = useState(0);
+
+    //Elegimos el número de preguntas que queremos
+    const handleAmoutChange = (e) => {
+        setAmountQuestions(e.target.value)
+    }
+
+    //Añadimos las preguntas
+    const addQuestions = () => {
+        axios.get(`https://opentdb.com/api.php?amount=${amountQuestions}`).then( res => {
+            console.log(res.data.results);
+            setQuestions(res.data.results)
+        })
+    }
+
+    //Mezclamos las respuestas
+    const mixAnswers = (answers) => {
+        const mixedAnswers = [...answers].sort(() => Math.random() - 0.5);
+        return mixedAnswers;
+    }
 
     useEffect(() => {
-        axios.get("https://opentdb.com/api.php?amount=10&type=multiple").then(res => {
-            console.log(res);
+        axios.get("https://opentdb.com/api.php?amount=50").then(res => {
+            console.log(res.data.results);
+            // console.log(res);
+            setQuestions(res.results)
         })
     }, [])
 
     return(
         <div>
-            su puta madre
+            <input type="number" name="amount" value={amountQuestions} onChange={handleAmoutChange}/>
+            <button onClick={addQuestions}>Añade el número de preguntas</button>
+            {questions && questions.map((question, index) => {
+                
+                const answers = [ ...question.incorrect_answers, question.correct_answer];
+                
+                const mixedAnswers = mixAnswers(answers)
+
+                return (
+                    <div key={index}>
+                    <h3>{question.question}</h3>
+                    <ul>
+                        {mixedAnswers.map((answers, i) => (
+                            <li key={i}>{answers}</li>
+                        ))}
+                    </ul>
+                </div>
+                )
+                
+            })}
+            
         </div>
     )
 }
